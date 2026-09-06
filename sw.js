8cac9b7ff4dae6a20f45d19e1607837f2183271e
Jeffacus <jd_stubbs@hotmail.com>
2026-09-06 15:50:44 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,1 @@
-const CACHE='wufc-u13-v1';
-const ASSETS=['./','./index.html','./styles.css','./data.js','./app.js','./manifest.json','./assets/club-badge.png','./assets/westerhope-header.png','./assets/u13-silvers-header.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+// Temporarily disabled while the Westerhope site is being developed.
